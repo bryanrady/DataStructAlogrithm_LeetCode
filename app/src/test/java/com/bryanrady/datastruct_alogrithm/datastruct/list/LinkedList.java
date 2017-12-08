9b4849aab7359228b360605d47d0e0789f098253
bryanrady <bryanrady@163.com>
2017-12-08 14:34:30 +0800

    数据结构：手写实现LinkedList的基本方法，了解其中实现原理，LinkedList当做栈、队列的使用方法

--- app/src/test/java/com/bryanrady/datastruct_alogrithm/datastruct/list/LinkedList.java
+++ app/src/test/java/com/bryanrady/datastruct_alogrithm/datastruct/list/LinkedList.java
@@ -1,6 +1,7 @@
 package com.bryanrady.datastruct_alogrithm.datastruct.list;
 
 import java.util.Collection;
+import java.util.NoSuchElementException;
 
 /**
  *  class LinkedList<E> extends AbstractSequentialList<E>
@@ -26,6 +27,38 @@ public class LinkedList<E> {
     public LinkedList(Collection<? extends E> c){
         this();
         addAll(c);
+    }
+
+    /**
+     * 数据结构
+     * @param <E>
+     */
+    static class Node<E>{
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev,E element,Node next){
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 返回LinkedList的大小
+     * @return
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
+     * 判断集合是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return size == 0;
     }
 
     /**
@@ -68,7 +101,7 @@ public class LinkedList<E> {
             }else{
                 pred.next = newNode;
             }
-            //此时把要插入的节点的上一个节点变成刚刚插入的节点
+            //此时把下一个要插入的节点的上一个节点变成刚刚插入的节点
             pred = newNode;
         }
         /****插入完成***/
@@ -92,6 +125,36 @@ public class LinkedList<E> {
     public boolean add(E e){
         linkLast(e);
         return true;
+    }
+
+    /**
+     * 在指定的位置往集合中添加元素e
+     * @param index
+     * @param e
+     */
+    public void add(int index,E e){
+        checkPositionIndex(index);
+        if(index == size){
+            linkLast(e);
+        }else{
+            linkBefore(e,node(index));
+        }
+    }
+
+    /**
+     * 添加元素作为第一个节点
+     * @param e
+     */
+    public void addFirst(E e){
+        linkLast(e);
+    }
+
+    /**
+     * 添加元素作为最后一个节点
+     * @param e
+     */
+    public void addLast(E e){
+        linkLast(e);
     }
 
     /**
@@ -144,7 +207,198 @@ public class LinkedList<E> {
         size++;
     }
 
-    //unlink准备
+    /**
+     * 删除元素，默认删除第一个,并返回
+     * @return
+     */
+    public E remove(){
+        return removeFirst();
+    }
+
+    /**
+     * 删除集合中指定位置的元素
+     * @param index
+     * @return
+     */
+    public E remove(int index){
+        checkElementIndex(index);
+        return unlink(node(index));
+    }
+
+    /**
+     * 在集合中删除指定的对象
+     * @param o
+     * @return
+     */
+    public boolean remove(Object o){
+        if(o == null){
+            for(Node<E> x=first;x!=null;x=x.next){
+                if(x.element == null){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }else{
+            for(Node<E> x=first;x!=null;x=x.next){
+                if(x.element.equals(o)){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 删除链表的第一个元素并返回
+     * @return
+     */
+    public E removeFirst(){
+        Node<E> f = first;
+        if(f == null){
+            throw new NoSuchElementException();
+        }
+        return unlinkFirst(f);
+    }
+
+    /**
+     * 删除链表的最后一个元素，并返回
+     * @return
+     */
+    public E removeLast(){
+        Node<E> l = last;
+        if(l == null){
+            throw new NoSuchElementException();
+        }
+        return unlinkLast(l);
+    }
+
+    /**
+     * 清空链表     循环遍历全部置为null
+     */
+    public void clear(){
+        for(Node<E> x=first;x!=null;x=x.next){
+            x.element =null;
+            x.prev = null;
+            x.next = null;
+            x = null;
+        }
+        first = last = null;
+        size = 0;
+    }
+
+    /**
+     * 删除一个非空节点x，并返回
+     * @param x
+     * @return
+     */
+    private E unlink(Node<E> x){
+        E element = x.element;
+        Node<E> pred = x.prev;
+        Node<E> succ = x.next;
+        if(pred == null){
+            first = succ;
+        }else{
+            pred.next = succ;
+            x.prev = null;
+        }
+        if(succ == null){
+            last = pred;
+        }else{
+            succ.prev = pred;
+            x.next = null;
+        }
+        x.element = null;
+        size--;
+        return element;
+    }
+
+    /**
+     * 删除不为空的第一个节点
+     * @param f
+     */
+    private E unlinkFirst(Node<E> f){
+        E element = f.element;
+        Node<E> succ = f.next;
+        f.element = null;     //help gc
+        f.next = null;
+        first = succ;
+        if(succ == null){
+            last = null;
+        }else{
+            succ.prev = null;
+        }
+        size--;
+        return element;
+    }
+
+    /**
+     * 删除不为空的最后一个节点
+     * @param l
+     * @return
+     */
+    private E unlinkLast(Node<E> l){
+        E element = l.element;
+        Node<E> pred = l.prev;
+        l.element = null;
+        l.prev = null;
+        last = pred;
+        if(pred == null){
+            first = null;
+        }else{
+            pred.next = null;
+        }
+        size--;
+        return element;
+    }
+
+    /**
+     * 修改在链表中指定位置的元素
+     * @param index
+     * @param e
+     * @return
+     */
+    public E set(int index,E e){
+        checkElementIndex(index);
+        Node<E> oldNode = node(index);
+        E oldVal = oldNode.element;
+        oldNode.element = e;
+        return oldVal;
+    }
+
+    /**
+     * 根据index获取集合中的指定的元素
+     * @param index
+     * @return
+     */
+    public E get(int index){
+        checkElementIndex(index);
+        return node(index).element;
+    }
+
+    /**
+     * 获取第一个节点上的元素
+     * @return
+     */
+    public E getFirst(){
+        Node<E> f = first;
+        if(f == null){
+            throw new NoSuchElementException();
+        }
+        return f.element;
+    }
+
+    /**
+     * 获取最后一个节点的元素
+     * @return
+     */
+    public E getLast(){
+        Node<E> l = last;
+        if(l == null){
+            throw new NoSuchElementException();
+        }
+        return l.element;
+    }
 
     /**
      * 根据index位置获取节点
@@ -169,11 +423,80 @@ public class LinkedList<E> {
     }
 
     /**
+     * 判断链表集合中是否包含此对象
+     * @param o
+     * @return
+     */
+    public boolean contains(Object o){
+        return indexOf(o) >= 0;
+    }
+
+    /**
+     * 获取对象o在链表集合中的索引位置
+     * @param o
+     * @return
+     */
+    public int indexOf(Object o){
+        int index = 0;
+        if(o == null){
+            for(Node<E> x=first;x!=null;x=x.next){
+                if(x.element == null){
+                    return index;
+                }
+                index++;
+            }
+        }else{
+            for(Node<E> x=first;x!=null;x=x.next){
+                if(x.element.equals(o)){
+                   return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 返回此对象在链表中最后出现的位置
+     * @param o
+     * @return
+     */
+    public int lastIndexOf(Object o){
+        int index = size-1;
+        if(o == null){
+            for(Node<E> x=last;x!=null;x=x.prev){
+                if(x.element == null){
+                    return index;
+                }
+                index--;
+            }
+        }else{
+            for(Node<E> x=last;x!=null;x=x.prev){
+                if(x.element.equals(o)){
+                    return index;
+                }
+                index--;
+            }
+        }
+        return -1;
+    }
+
+    /**
      *  添加元素的时候检查索引位置
      * @param index
      */
     private void checkPositionIndex(int index){
         if(index<0 || index>size) {
+            throw new IndexOutOfBoundsException("Index:" + index + ",Size:" + size);
+        }
+    }
+
+    /**
+     * 在删除和获取元素时判断检查下标位置
+     * @param index
+     */
+    private void checkElementIndex(int index){
+        if(index<0 || index>=size) {
             throw new IndexOutOfBoundsException("Index:" + index + ",Size:" + size);
         }
     }
@@ -191,16 +514,103 @@ public class LinkedList<E> {
         return o;
     }
 
-    static class Node<E>{
-        E element;
-        Node<E> prev;
-        Node<E> next;
+    /***    LinkedList实现了Deque<E>接口，Deque<E>接口对Queue<E>接口进行了扩展</>
+     * </>  LinkedList作为队列使用
+     *      一般用LinkedList作为队列使用较多，使用栈的时候最好还是使用Stack<E>这个类
+     * </>*****/
 
-        public Node(Node<E> prev,E element,Node next){
-            this.prev = prev;
-            this.element = element;
-            this.next = next;
+    /**
+     * 往队列尾部添加元素
+     * @param e
+     * @return
+     */
+    public boolean offer(E e){
+        return add(e);
+    }
+
+    /**
+     * 从队列头部取元素,并删除
+     * @return
+     */
+    public E poll(){
+        Node<E> f = first;
+        if(f == null){
+            return null;
         }
+        return unlinkFirst(f);
+    }
+
+    /**
+     * 从队列头部读取元素，但是不删除
+     * @return
+     */
+    public E peek(){
+        Node<E> f = first;
+        if(f == null){
+            return null;
+        }
+        return f.element;
+    }
+
+    public boolean offerFrist(E e){
+        addFirst(e);
+        return true;
+    }
+
+    public boolean offerLast(E e){
+        addLast(e);
+        return true;
+    }
+
+    public E pollFirst(){
+        Node<E> f = first;
+        if(f == null){
+            return null;
+        }
+        return unlinkFirst(f);
+    }
+
+    public E pollLast(){
+        Node<E> f = first;
+        if(f == null){
+            return null;
+        }
+        return unlinkLast(f);
+    }
+
+    public E peekFirst(){
+        Node<E> f = first;
+        if(f == null){
+            return null;
+        }
+        return f.element;
+    }
+
+    public E peekLast(){
+        Node<E> l = last;
+        if(l == null){
+            return null;
+        }
+        return l.element;
+    }
+
+    /***    LinkedList还可以当成是栈来使用  *****/
+
+    /**
+     * 往栈顶添加元素,就是在链表前面添加元素作为第一个节点
+     * @param e
+     */
+    public void push(E e){
+        addFirst(e);
+    }
+
+    /**
+     * 往栈顶取元素并删除，即删除链表前面删除元素，这样在就满足了栈的先进后出的原则
+     *  还有个不删除的就是和peek（）方法一样
+     * @return
+     */
+    public E pop(){
+        return removeFirst();
     }
 
 }
