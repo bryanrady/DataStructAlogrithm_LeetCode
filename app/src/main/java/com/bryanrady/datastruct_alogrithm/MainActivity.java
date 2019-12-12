2c2851f99129de54ad0ef1d2d1d560fe1090f65d
bryanrady <bryanrady@163.com>
2019-12-12 17:07:42 +0800

    改一下工程。

--- app/src/main/java/com/bryanrady/datastruct_alogrithm/MainActivity.java
+++ app/src/main/java/com/bryanrady/datastruct_alogrithm/MainActivity.java
@@ -1,7 +1,8 @@
 package com.bryanrady.datastruct_alogrithm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
